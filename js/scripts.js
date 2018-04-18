be16846e825771105ff744e488d882ba309c22dd
Fidel <jrekmecchia@gmail.com>
2018-04-18 19:28:45 -0300

    Fidel

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var users = [
+    {name:"Julian",
+    lastname:"Maruca",
+    age:23,
+    city:"Gonnet",
+    children:["Manuel","Pedro","Julian Jr"]},
+    {name:"Nico",
+    lastname:"Cacavo",
+    age:24,
+    city:"Villa Elvira",
+    children:["Nico Jr","Marcelo","Jose"]},
+    {name:"Fidel",
+    lastname:"Mecchia",
+    age:8,
+    city:"Cañada Rosquín",
+    children:["Renzo","Franco","Ignacio"]},
+    {name:"Siro",
+    lastname:"Benegas",
+    age:4,
+    city:"Rosario",
+    children:["Sirito Jr","Jeremias","Lucas"]},
+]
+for (var i=0 ; i< users.length ; i++){
+    console.log(lastname,name tiene children);
+}
